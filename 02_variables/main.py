dfd36820320874b205c02168829f2c413dfcc3e3
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-26 16:59:56 +0530

    Create main.py

---
+++ 02_variables/main.py
@@ -0,0 +1,8 @@
+#Python, variables are used to store data that can be used and manipulated throughout the a program. 
+#A variable is created that moment you assign value to it using assignment variable(=)
+
+age = 24
+name = "aswal"
+cgpa = 9.00
+
+
