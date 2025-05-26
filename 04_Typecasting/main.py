d555579cd74632fe0f11a747000beac37dfcc995
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-26 17:19:18 +0530

    Create main.py

---
+++ 04_Typecasting/main.py
@@ -0,0 +1,23 @@
+a = 34
+b = "34"
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#convert b into integer
+
+c = int(b)
+
+print(c)
+print(type(c))
+
+d = 23
+
+print(d)
+print(type(d))
+
+e = str(d)
+print(e)
+print(type(e))
