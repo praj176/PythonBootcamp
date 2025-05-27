fa778e82d9daada0984f8890f21f512c524c145d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-27 16:21:08 +0530

    Create escape_char.py

---
+++ 04_comment_escchar/escape_char.py
@@ -0,0 +1,10 @@
+# Run this first
+# print("Escape 
+#       character")
+
+print("Hey how are you? \n I am good, thanks for asking!")
+
+print("Hey how are you? \t I am good, thanks for asking!")
+print("Hey how are you? \\ I am good, thanks for asking!")
+print("Hey how are you? \' I am good, thanks for asking!")
+print("Hey how are you? \" I am good, thanks for asking!")
