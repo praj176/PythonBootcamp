d4b597d4388a6b2ab32875575d1df7ef3b8ea27d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-28 16:08:18 +0530

    Create 01_def.py

---
+++ 10_Functions/01_def.py
@@ -0,0 +1,10 @@
+#functions help in reusability and modularity in Python
+
+def average(a,b,c):
+    avg = (a + b + c) /3
+    # print(avg)
+    return avg # the return statement is used inside a function to send a value back to the caller. When a function reaches a return statement, it stops execution and returns the specified value.
+
+a = average(3,1,4)
+
+
