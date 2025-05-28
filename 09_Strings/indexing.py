b6de3790640a268cd1a2b54d9df395849a3950ef
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-28 16:06:04 +0530

    Create indexing.py

---
+++ 09_Strings/indexing.py
@@ -0,0 +1,14 @@
+name = "Taklu Haiwan"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print()
+print(name[-1])
+print(name[-2])
+print(name[-3])
+print(name[-4])
+print(name[-5])
+print(name[-6])
+
