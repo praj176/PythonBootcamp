891315fa31f89574fae87148a078c1ac5a028d7a
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-28 16:04:11 +0530

    Create continue.py

---
+++ 08_Loops/continue.py
@@ -0,0 +1,9 @@
+for i in range(0,21):
+    if i == 10:
+        continue #continue the loop for the next iteration here itself >> Halt the execution for this particular iteration
+    print(i)
+
+#pass statement is the placeholder that does nothing. it is used when syntax requires a statement but no action is required.
+
+for i in "hello":
+    print(i)
