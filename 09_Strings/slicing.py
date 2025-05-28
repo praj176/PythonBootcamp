80cab62c8eb630f50c0a40b35d234c76b7a75aa8
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-28 16:05:45 +0530

    Create slicing.py

---
+++ 09_Strings/slicing.py
@@ -0,0 +1,19 @@
+name = "BabuKalia"
+
+print(name[0:4]) #goes from 0 to 4-1 index
+
+print(name[2:-1]) # convert negative to positive index : -1 + (len of string) 9 = 8
+
+# print(name[0:10:n]) #skip n - 1 characters
+
+print(name[0:10:1]) #skip 1-1 = 0 characters in between
+
+print(name[0:10:2]) # skip 2-1 = 1 characters in between
+print(name[0:10:4]) # skip 4-1 = 3 characters in between
+
+
+print(name[:4]) # Replace the first empty number with 0 # name[0:4]
+
+print(name[1:]) # Replace the second empty number with len # name[1:9]
+
+
