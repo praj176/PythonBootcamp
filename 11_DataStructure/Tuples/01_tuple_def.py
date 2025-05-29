c5eb38ea9ac5140440e2a7d98a9a1ec2a92cf629
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:47:04 +0530

    Create 01_tuple_def.py

---
+++ 11_DataStructure/Tuples/01_tuple_def.py
@@ -0,0 +1,13 @@
+# Orders and Immutable(cannot be changed after creation) collection of items.
+
+#Create a tuple >>> use parenthesis
+
+tupple = (22,44,11,24,12)
+
+print(tupple)
+print(tupple[3])
+### How to create at tuple with just 1 element
+
+single = (2,)
+
+print(single)
