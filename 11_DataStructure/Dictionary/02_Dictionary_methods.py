fcdd6997f461a8214b842e0d2a7b9a6aa1980541
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:50:12 +0530

    Create 02_Dictionary_methods.py

---
+++ 11_DataStructure/Dictionary/02_Dictionary_methods.py
@@ -0,0 +1,10 @@
+student = {"name": "Richard", "age": 34, "grade": "A"}
+
+print(student.keys()) # prints all the keys present in the dictionary
+
+print(student.values()) #prints all the values present in the dictionary
+
+student.pop("name") #Pops/ deletes the KEY and the associated value given to the pop function
+# student.clear() # Clears all the dictionary elements
+print(student)
+
