689f50c5f47717ee9660cc86ac4b1d95d023353a
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:47:40 +0530

    Create 02_Tuple_unpacking.py

---
+++ 11_DataStructure/Tuples/02_Tuple_unpacking.py
@@ -0,0 +1,13 @@
+#Tuple unpacking in Python is a technique that allows you to assign multiple values from a tuple to multiple variables in a single step. This makes your code cleaner and more readable.
+
+# Defining a tuple
+person = ("Alice", 25, "Engineer")
+
+# Unpacking the tuple into variables
+name, age, profession = person
+
+print(name)       # Output: Alice
+print(age)        # Output: 25
+print(profession) # Output: Engineer
+print(name, age, profession)
+print(len(person))
