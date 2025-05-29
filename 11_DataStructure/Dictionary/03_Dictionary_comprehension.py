54afdf97a86cf3e9a5d2ca232e24c21f549885c9
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:50:40 +0530

    Create 03_Dictionary_comprehension.py

---
+++ 11_DataStructure/Dictionary/03_Dictionary_comprehension.py
@@ -0,0 +1,8 @@
+#similar to list comprehension we have Dictionary comprehension
+
+table_of_5 = {i: 5*i for i in range(1,11)}
+
+print(table_of_5)
+
+squares = {i: i**2 for i in range(1,11)}
+print(squares)
