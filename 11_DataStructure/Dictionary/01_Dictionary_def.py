e5967b9193618e57253bfb0d5c4c21fe2038d071
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:49:43 +0530

    Create 01_Dictionary_def.py

---
+++ 11_DataStructure/Dictionary/01_Dictionary_def.py
@@ -0,0 +1,11 @@
+student = {"name": "Richard", "age": 34, "grade": "A"}
+
+print(student["name"])
+
+#adding new element
+
+student["class"] = "Jupyter"
+
+#Udpates the old element
+student["age"] = 23
+print(student)
