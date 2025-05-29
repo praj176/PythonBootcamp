8cea56f23b07168b0534b4a7be3f3228333590f2
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-05-29 13:46:03 +0530

    Create 02_list_methods.py

---
+++ 11_DataStructure/Lists/02_list_methods.py
@@ -0,0 +1,12 @@
+marks = [54,12,98,23,76,12,67]
+names = ["Pankaj","Dev"]
+print(marks)
+print(marks.append(99))
+print(marks)
+print(marks.pop(3))
+print(marks)
+print(marks.insert(0,"noob"))
+print(marks)
+
+a = marks.extend(names)
+print(a)
