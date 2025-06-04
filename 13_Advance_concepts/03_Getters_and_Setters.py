abcf1d03acb3df070206e3a66b91bb7c98a1ea79
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-04 13:55:49 +0530

    Update 03_Getters_and_Setters.py

--- 13_Advance_concepts/03_Getters_and_Setters.py
+++ 13_Advance_concepts/03_Getters_and_Setters.py
@@ -1,1 +1,10 @@
+'''
+Setters and Getter used for writting cleaner syntax.
+Setters and getters are special methods used in object-oriented programming to control access to class attributes.
 
+> Getter: A method that retrieves (gets) the value of an attribute.
+Setter: A method that modifies (sets) the value of an attribute while ensuring valid data.
+
+They help protect data, enforce constraints, and maintain clean code.
+
+'''
