8571ca73e831db3887e453e33185e38b49780b90
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-06 23:42:22 +0530

    Update 04_instance_static_class.py

--- 13_Advance_concepts/04_instance_static_class.py
+++ 13_Advance_concepts/04_instance_static_class.py
@@ -1,1 +1,13 @@
+class Employee:
+    company = "HP"
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        
+e1 = Employee("Jack", 34500)
+e2 = Employee("Jill", 36500)
 
+print(Employee.company) #Class attribute / variable
+
+print(Employee.name) #it is the attribute for the object/instance
+        
