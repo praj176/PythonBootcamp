757394367d94216a7302de7ebbea45e6b9283c49
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-09 23:27:39 +0530

    Update 05_Dunder_Methods.py

--- 13_Advance_concepts/05_Dunder_Methods.py
+++ 13_Advance_concepts/05_Dunder_Methods.py
@@ -1,1 +1,5 @@
-
+'''
+Dunder methods (also called magic methods) are special methods in Python with names having double underscores like __init__, __str__, and __add__.
+  > They let you customize how your objects behave with built-in operators, functions, and language features.
+  > They make your classes more intuitive, powerful, and Python —supporting things like operator overloading, custom string output, and more.
+'''
