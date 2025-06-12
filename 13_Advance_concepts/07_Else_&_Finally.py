4ce4d180db0a110a0916b4bbbd4fe7bca83b214d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-12 23:45:29 +0530

    Update 07_Else_&_Finally.py

--- 13_Advance_concepts/07_Else_&_Finally.py
+++ 13_Advance_concepts/07_Else_&_Finally.py
@@ -1,1 +1,13 @@
+try:
+  a = 23/0
 
+except Exception as e:
+  print(e)
+
+#Gets executed when there is no error in the try block
+else:
+  print("Code works fine")
+
+'''
+You won’t get the output from the else block because the try block raises an error, so the else block is skipped.
+'''
