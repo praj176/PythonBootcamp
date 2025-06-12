cf5feb01010bc815f52c8b890496f3fc1949fa3b
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-12 23:38:18 +0530

    Update 06.1_Error_raising.py

--- 13_Advance_concepts/06.1_Error_raising.py
+++ 13_Advance_concepts/06.1_Error_raising.py
@@ -1,3 +1,12 @@
+'''
+Raising/Throwing an error
+
+'''
+
 a = int(input("Enter the first number? "))
 b = int(input("Enter the second number? "))
+
+#before the error occurs
+if b == 0 :
+  raise ValueError("Please don't divide by zero")
 print(f"The division of both number is {a / b}")
