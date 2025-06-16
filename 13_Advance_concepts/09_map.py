ad87393755a30d09cc1b866559d5a00be23bd5a1
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-16 15:30:02 +0530

    Update 09_map.py

--- 13_Advance_concepts/09_map.py
+++ 13_Advance_concepts/09_map.py
@@ -1,6 +1,11 @@
 '''
 Map, Filter and Reduce are higher-order functions in Python which operate on iterables (LIST, TUPLES, etc)
+
+> map(), filter(), and reduce() are powerful built-in functions in Python that let you process and transform data in elegant, functional ways.
 '''
+
+# map(function, iterable)
+# Applies a function to every item in an iterable (like a list) and returns a map object (which can be converted to a list).
 
 numbers = [1,2,3,4,12,34,25]
 
