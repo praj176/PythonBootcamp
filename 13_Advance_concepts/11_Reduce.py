d5dc9cac5efe0bb7483440a02ddad8280223bc43
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-16 15:35:15 +0530

    Update 11_Reduce.py

--- 13_Advance_concepts/11_Reduce.py
+++ 13_Advance_concepts/11_Reduce.py
@@ -1,1 +1,5 @@
-
+'''
+reduce(function, iterable) :
+Takes a list and combines its items one by one into a single value using a function you provide.
+Applies a rolling computation to sequential pairs of items.
+'''
