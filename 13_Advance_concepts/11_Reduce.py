22fe827059c96d3f812e38a7ded4c30fd109658d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-16 15:49:46 +0530

    Update 11_Reduce.py

--- 13_Advance_concepts/11_Reduce.py
+++ 13_Advance_concepts/11_Reduce.py
@@ -3,3 +3,12 @@ reduce(function, iterable) :
 Takes a list and combines its items one by one into a single value using a function you provide.
 Applies a rolling computation to sequential pairs of items.
 '''
+from functools import reduce
+
+list = [1,2,3,4,5,6]
+
+def sum(a,b):
+  return a + b
+
+c = reduce(sum, list)
+print(c)
