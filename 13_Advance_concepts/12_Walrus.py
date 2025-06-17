ec7275ac55cd8bbde2fa734ae92a198bc57c7cf5
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 12:53:07 +0530

    Update 12_Walrus.py

--- 13_Advance_concepts/12_Walrus.py
+++ 13_Advance_concepts/12_Walrus.py
@@ -2,3 +2,13 @@
 Walrus operator is := is an assignment expression operator which allows you to assign a value to a variable within an expression
 > makes code concise, avoids repeated function calls
 '''
+'''
+>> Without walrus operator
+
+list = [1, 2, 3, 4, 5]
+n = len(list)
+while n > 0:
+    print(numbers.pop())
+    n = len(numbers)
+'''
+
