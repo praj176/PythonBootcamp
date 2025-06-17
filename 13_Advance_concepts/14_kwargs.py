bcff94d99e8dc7fa6989fcd51f0a4cb4d9d70c3c
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 14:50:33 +0530

    Update 14_kwargs.py

--- 13_Advance_concepts/14_kwargs.py
+++ 13_Advance_concepts/14_kwargs.py
@@ -1,1 +1,12 @@
+'''
+**kwargs allows passing multiple keyword arguments.
+
+The arguments are stored as a dictionary. In a form of key-value pair
+'''
+
+def print_details(**kwargs):
+    for key, value in kwargs.items(): #.items() method in Python returns a view object that contains key-value pairs from a dictionary as tuples.
+        print(f"{key}: {value}")
+
+print_details(name="Alice", age=25, city="New York")
 
