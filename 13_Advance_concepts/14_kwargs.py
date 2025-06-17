e54f24304b9d53d60f8cd941f8e0a60e8d121d7d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 15:03:09 +0530

    Update 14_kwargs.py

--- 13_Advance_concepts/14_kwargs.py
+++ 13_Advance_concepts/14_kwargs.py
@@ -5,7 +5,7 @@ The arguments are stored as a dictionary. In a form of key-value pair
 '''
 
 def print_details(**kwargs):
-    for key, value in kwargs.items(): #.items() method in Python returns a view object that contains key-value pairs from a dictionary as tuples.
+    for key, value in kwargs.items(): #.items() method returns a view of key-value pairs as tuples.
         print(f"{key}: {value}")
 
 print_details(name="Alice", age=25, city="New York")
