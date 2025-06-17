49bb244c67305283671dbdeb544b5a6a32de6c50
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 12:51:50 +0530

    Update 12_Walrus.py

--- 13_Advance_concepts/12_Walrus.py
+++ 13_Advance_concepts/12_Walrus.py
@@ -1,1 +1,4 @@
-
+'''
+Walrus operator is := is an assignment expression operator which allows you to assign a value to a variable within an expression
+> makes code concise, avoids repeated function calls
+'''
