3fe8ffed44621a422d281028c1e9a90e2558eab5
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 14:47:19 +0530

    Update 13_Args.py

--- 13_Advance_concepts/13_Args.py
+++ 13_Advance_concepts/13_Args.py
@@ -3,3 +3,12 @@
 '''
 # *args lets you pass multiple positional arguments to a function.The arguments are stored as a tuple.
 # **kwargs allows passing multiple keyword arguments. The arguments are stored as a dictionary.
+
+def sum(*args):
+    print(f"These are the arguments {args}.")
+    total = 0
+    for i in args:
+        total += i
+    return total
+
+print(sum(2,5,21,54,23))
