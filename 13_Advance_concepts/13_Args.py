1ec0d0d410cf97d7af4dd87fe401c3c9668bddde
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 14:27:13 +0530

    Info regarding args and kwargs

--- 13_Advance_concepts/13_Args.py
+++ 13_Advance_concepts/13_Args.py
@@ -1,1 +1,5 @@
-
+'''
+*args and **kwargs allow functions to accept a variable number of arguments.
+'''
+# *args lets you pass multiple positional arguments to a function.The arguments are stored as a tuple.
+# **kwargs allows passing multiple keyword arguments. The arguments are stored as a dictionary.
