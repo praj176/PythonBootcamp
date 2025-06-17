a4b5df8ffcbefcc6ade1fcc8a8c792b4a4a0a6db
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 15:15:13 +0530

    Update 14_kwargs.py with output and comments

--- 13_Advance_concepts/14_kwargs.py
+++ 13_Advance_concepts/14_kwargs.py
@@ -4,9 +4,26 @@
 The arguments are stored as a dictionary. In a form of key-value pair
 '''
 
-def print_details(**kwargs):
-    for key, value in kwargs.items(): #.items() method returns a view of key-value pairs as tuples.
-        print(f"{key}: {value}")
+# def print_details(**kwargs):
+#     for key, value in kwargs.items(): 
+# #   The .items() method in Python returns key-value pairs from a dictionary as tuples in a view object.
+#         print(f"{key}: {value}")
 
-print_details(name="Alice", age=25, city="New York")
+# print_details(name="Alice", age=25, city="New York")
 
+def school(**kwargs):
+    #kwargs is a dictionary with all the key value pair which is passed to kwargs
+    print(kwargs.keys())
+    for item in kwargs.keys():
+        print(f"Parameters are {item} and value for them are {kwargs[item]}") 
+        #kwargs[item(#which are the keys in this case)] retrieves the value associated with that key.
+        
+school(name = "Jeju", age = 45, location = "Nashik")
+
+'''
+Output :
+dict_keys(['name', 'age', 'location'])
+Parameters are name and value for them are Prajwal
+Parameters are age and value for them are 45
+Parameters are location and value for them are Nashik
+'''
