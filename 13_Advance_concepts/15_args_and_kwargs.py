daa38e20cabf0e4578012673fa4ca9886a86b82c
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-17 15:25:20 +0530

    Update 15_args_and_kwargs.py

--- 13_Advance_concepts/15_args_and_kwargs.py
+++ 13_Advance_concepts/15_args_and_kwargs.py
@@ -1,1 +1,14 @@
 # We can use args and kwargs together with one single condition > args should come before kwargs
+
+def fun1(*args,**kwargs):
+    print(f"This all are args : {args}")
+    print(f"This all are args : {kwargs}")
+    
+    
+fun1(3,1,5,9,"akka", Name= "issmile", age = 23, location = "Nanded")
+
+'''
+Output :
+This all are args : (3, 1, 5, 9, 'akka')
+This all are args : {'Name': 'issmile', 'age': 23, 'location': 'Nanded'}
+'''
