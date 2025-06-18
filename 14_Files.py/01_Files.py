140ef8ec4c047c84f38b76a6a0d75377238a6a4a
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-18 18:18:31 +0530

    Update 01_Files.py

--- 14_Files.py/01_Files.py
+++ 14_Files.py/01_Files.py
@@ -1,3 +1,15 @@
 '''
-Read append and close the file
+We can read-write and append a file
+Working with files generally involves:
+1. Opening a file
+2. Performing operations
+3. Closing the file
 '''
+#in-order to read a file we need to create a file object
+f = open("14_Files.py\test.txt", "r")
+
+content = f.read()
+
+print(content)
+
+f.close()
