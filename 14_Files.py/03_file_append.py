0e0bb98946a8d2397c5afcab13ea52518b0077e5
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-18 18:39:56 +0530

    Update 03_file_append.py

--- 14_Files.py/03_file_append.py
+++ 14_Files.py/03_file_append.py
@@ -1,1 +1,9 @@
+#Opens the file for appending. Data will be added to the end of the file. If the file doesn't exist new file will be created. 
+#Append to an existing file called test.txt file
+# add more info regarding random info
 
+f = open("14_File_IO.py\qew.txt", "a")
+
+string = '''Jon doe is a engineer. working in a good company as data science expert. he lives in Johanesburg'''
+f.write(string)
+f.close()
