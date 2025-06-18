b4d59fdb67612c8df71198f408a945d1fa4afcc0
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-18 18:29:05 +0530

    Update 02_file_write.py

--- 14_Files.py/02_file_write.py
+++ 14_Files.py/02_file_write.py
@@ -1,3 +1,6 @@
+# Opens the file for writing. If the file exists, its contents will be overwritten 
+#(If the file doesn't exist, a new file will be created)
+
 # write info in the text file
 
 f = open("14_File_IO.py\qew.txt", "w")
