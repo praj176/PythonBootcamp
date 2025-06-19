50db8658eba759344093f3e8f9f4072eb013a9ed
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-20 00:13:47 +0530

    Update 02_file_write.py

--- 14_Files.py/02_file_write.py
+++ 14_Files.py/02_file_write.py
@@ -3,8 +3,8 @@
 
 # write info in the text file
 
-f = open("14_File_IO.py\qew.txt", "w")
-
+f = open("14_File_IO.py\\qew.txt", "w")
+#Use relative path or the absolute path of the file and at last try to use double backward slashes (\\) instead of one to avoid Syntax errors for escape characters 
 string = '''Jon doe is a good guy he is a guy who is good!'''
 f.write(string)
 f.close()
