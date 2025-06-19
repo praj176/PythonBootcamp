fb58fae84358b5053507b8457846ec4c66a26ee3
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-20 00:21:59 +0530

    Update 04_with.py

--- 14_Files.py/04_with.py
+++ 14_Files.py/04_with.py
@@ -1,1 +1,13 @@
+# >> Normal method to read a file
+# f = open("14_File_IO.py\\qew.txt", "r")
+# content = f.read()
+# print(content)
+# f.close()
+
+#Better approach
+with open("14_File_IO.py//openme.txt","r") as f:
+    content = f.read()
+    print(content)
+    
+# No need to write f.close() Because file is automatically closed using the with.
 
