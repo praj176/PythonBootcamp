74ed75caac8091b6096e8b6178244036e2255a08
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-20 00:14:01 +0530

    Update 03_file_append.py

--- 14_Files.py/03_file_append.py
+++ 14_Files.py/03_file_append.py
@@ -3,7 +3,8 @@
 #Append to an existing file called test.txt file
 # add more info regarding random info
 
-f = open("14_File_IO.py\qew.txt", "a")
+f = open("14_File_IO.py\\qew.txt", "a")
+#Use relative path or the absolute path of the file and at last try to use double backward slashes (\\) instead of one to avoid Syntax errors for escape characters 
 
 string = '''Jon doe is a engineer. working in a good company as data science expert. he lives in Johanesburg'''
 f.write(string)
