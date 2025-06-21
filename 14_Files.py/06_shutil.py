a7bc9c936d57b6925f25767e57bbfb34daa87f35
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-21 23:54:57 +0530

    Update 06_shutil.py

--- 14_Files.py/06_shutil.py
+++ 14_Files.py/06_shutil.py
@@ -1,1 +1,17 @@
+'''
+The shutil module in is used for file and directory operations — especially copying, moving, deleting, and archiving.
+'''
+import shutil
 
+# Copy a file
+shutil.copy("example.txt", "backup.txt")
+
+# Move a file
+shutil.move("backup.txt", "folder/backup.txt")
+
+# Delete a folder
+shutil.rmtree("folder")
+
+# Get disk usage
+usage = shutil.disk_usage("/")
+print(f"Free: {usage.free // (1024**3)} GB")
