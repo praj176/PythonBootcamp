130fdc2d08903d3d0a14ed44c8b98139a264896b
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-23 21:02:26 +0530

    Update 07_Command_Line_Util.py

--- 14_Files.py/07_Command_Line_Util.py
+++ 14_Files.py/07_Command_Line_Util.py
@@ -9,3 +9,11 @@ A command-line utility is a Python script you run from the terminal, like built-
 
 eg. pip install pandas → pip is a command-line utility
 '''
+# greet.py
+import argparse
+
+parser = argparse.ArgumentParser(description="Greets the user")
+parser.add_argument('--name', type=str, help='Your name')
+args = parser.parse_args()
+
+print(f"Hello, {args.name}!")
