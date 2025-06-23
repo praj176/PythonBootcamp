fe5a4519e12495243e0204f1289f5448a3ac1aef
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-23 20:53:27 +0530

    Update 07_Command_Line_Util.py

--- 14_Files.py/07_Command_Line_Util.py
+++ 14_Files.py/07_Command_Line_Util.py
@@ -1,1 +1,5 @@
+'''
+A command-line utility is a Python script you can run from the terminal/command prompt, just like built-in commands (ls, cd, mkdir, etc.), but made by you.
 
+It accepts arguments and options from the command line, and then performs some task — like file conversion, automation, system checks, etc.
+'''
