4b13ec14219c49f540f585e9d6cdcee2b7605e1d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-24 15:39:25 +0530

    Update 01_virtualenv.py

--- 15_External_Modules/01_virtualenv.py
+++ 15_External_Modules/01_virtualenv.py
@@ -1,1 +1,4 @@
-
+'''
+A Python virtual environment is an isolated workspace where you can install and manage packages separately from the system-wide Python. 
+It helps avoid version conflicts between projects.
+'''
