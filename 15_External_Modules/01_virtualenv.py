4f93d02e7f4b6b98c0cd8348bd3ec2c6c61f569c
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-06-24 18:06:29 +0530

    Update 01_virtualenv.py

--- 15_External_Modules/01_virtualenv.py
+++ 15_External_Modules/01_virtualenv.py
@@ -13,4 +13,18 @@ env_name\Scripts\Activate.ps1
 
 #Now we can install modules in this environment from scratch
 
-# pip stands for “Pip Installs Packages” is the default package manager for Python, used to install and manage additional libraries and dependencies that aren’t included in the standard Python library.
+# pip stands for “Pip Installs Packages” is the default package manager for Python 
+#It is used to install and manage additional libraries and dependencies that aren’t included in the standard Python library.
+
+pip install package_name
+
+pip install --upgrade package_name #Upgrading a package
+
+pip list # Lists all installed packages
+
+pip uninstall package_name #uninstall
+
+'''Requirements.txt'''
+
+pip freeze # list downs all the packages with versions
+
