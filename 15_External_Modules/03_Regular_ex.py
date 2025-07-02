48dc9405ce12c5240b27b90b79bd2e5b810e22e8
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-07-02 19:21:53 +0530

    Update 03_Regular_ex.py

--- 15_External_Modules/03_Regular_ex.py
+++ 15_External_Modules/03_Regular_ex.py
@@ -1,1 +1,8 @@
-
+'''
+Regular expressions (or regex) in Python are like powerful search tools for text—they help you find, match, or manipulate patterns in strings.
+You use it to:
+ > Check if a string contains a pattern
+ > Extract specific parts of a string
+ > Replace or split text based on patterns
+ 
+ '''
