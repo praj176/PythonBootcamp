c323e113c831a092764c1043e664d305dfe6c690
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-07-02 18:53:59 +0530

    Update 02_Requests_package.py

--- 15_External_Modules/02_Requests_package.py
+++ 15_External_Modules/02_Requests_package.py
@@ -8,5 +8,9 @@ import requests
 r = request.get("https://api.github.com")
 # will do the get request to get the information regarding the URL, 
 #requests.get("URL") > retrieve data from the URL
+print(r.text) # prints the data in the text data.
+with open("github.txt", "w") as f:
+  f.write(r.text)
+# Content of the URL is written the github.txt file
 
 
