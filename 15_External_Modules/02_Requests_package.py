81577d882beff6e60c55325f9dd8b094aeb15549
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-07-02 18:48:49 +0530

    Update 02_Requests_package.py

--- 15_External_Modules/02_Requests_package.py
+++ 15_External_Modules/02_Requests_package.py
@@ -1,3 +1,7 @@
 # Requests package helps Python talk to websites or APIs like asking for data or sending info.
 #Install Request package
-pip install requests
+pip install requests #terminal
+
+#python program
+import requests
+
