72ff23c9e1300263e06c5f763d38f3f1a83de912
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-07-03 19:43:15 +0530

    Update 04_Multithreading.py

--- 15_External_Modules/04_Multithreading.py
+++ 15_External_Modules/04_Multithreading.py
@@ -1,1 +1,12 @@
+'''
+Multi-threading allows your program to perform multiple tasks concurrently, which improves the performance
 
+> Threads and processes are like the building blocks of multitasking
+A process is an independent program in execution. It has it's own memory space, it's own resources (like file handles, variables, etc.)
+---> At least one thread running inside it
+
+> A thread is a smaller unit of execution within a process. Multiple threads in the same process:
+Share the same memory and resources. Can run tasks in parallel (like multitasking within the same app)
+
+Eg : Process can be considered as restaurant and threads can be considered as chefs.
+'''
