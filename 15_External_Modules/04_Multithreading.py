86fd65c8292be6755114ddf555e2f1b2e4960e3a
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-07-05 01:00:03 +0530

    Update 04_Multithreading.py

--- 15_External_Modules/04_Multithreading.py
+++ 15_External_Modules/04_Multithreading.py
@@ -20,3 +20,12 @@ def worker(num):
   print(f"Thread {num}: Starting")
   time.sleep(2)
   print(f"Thread {num}: Fninshing")
+
+threads = []
+for i in range(3):
+  thread = threading.Thread(target=worker, args=(i,))
+  threads.append(thread)
+  thread.start()
+for thread in threads:
+  thread.join() #waiting for all threads to finish
+print("all threads completed")
